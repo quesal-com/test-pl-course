34a82487a64a49c13162e830b9ec933c561bc4e5
Quesal <app@quesal.com>
2026-05-13 17:38:37 -0700

    Quesal sync for course "Test Course"

--- questions/exceptionHandlingControlFlow/util.py
+++
@@ -1,8 +0,0 @@
-import codecs
-
-def read_file(file):
-  return codecs.open(file, 'r').read()
-
-def write_file(filepath, content):
-  with open(filepath, 'w', encoding='utf-8') as file:
-    file.write(content)
